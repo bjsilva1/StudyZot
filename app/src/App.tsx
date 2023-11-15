555cb366af460bdbdce2cdc11e67026e4f99ec65
ailurada <dioxideshamrocksushi@gmail.com>
2023-11-15 11:01:57 -0800

    header and bootstrap

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import studyZotIcon from './assets/StudyZotIcon.svg'
+import studyZotIcon from './assets/StudyZotIcon_Circle.svg'
 import './App.css'
+
+function Switch() {
+  return (
+    <>
+      <label class="switch">
+        <input type="checkbox"/>
+        <span class="slider"></span>
+      </label>
+    </>
+  )
+}
 
 function TopNavBar() {
     return (
       <div class="navbar">
         <img src={studyZotIcon}/>
         <h1> StudyZot </h1>
-
+        <Switch />
       </div>
     )
 }
