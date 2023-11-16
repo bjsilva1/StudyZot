6cd71d97c291fe8af1462bdf7b0d4fb480348913
ailurada <dioxideshamrocksushi@gmail.com>
2023-11-16 13:12:31 -0800

    added filter sidebar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import { TopNavBar } from './components/TopNavBar.tsx'
 import { BasicStudyCard } from './components/SpaceCards.tsx'
+import { HeroCard } from './components/HeroCards.tsx'
+import { LeftSideBar } from './components/LeftSideBar.tsx'
+import Container from "react-bootstrap/Container"
 import './App.css'
 
 
@@ -8,10 +11,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Container className="big-container">
       <TopNavBar />
-      <BasicStudyCard />
-    </>
+      <Container className="bottom-container">
+        <LeftSideBar />
+        <Container className="cards-container">
+          <HeroCard />
+          <BasicStudyCard />
+        </Container>
+      </Container>
+    </Container>
   )
 }
 
