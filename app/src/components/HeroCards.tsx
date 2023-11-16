e408963af76265d831f17151d1c70ada5a5a806a
Bryan Silva <101739803+bjsilva1@users.noreply.github.com>
2023-11-16 14:40:30 -0800

    Update HeroCards.tsx

--- app/src/components/HeroCards.tsx
+++ app/src/components/HeroCards.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Card, Button, ProgressBar } from 'react-bootstrap'
 import DBHPlaceholder from '../assets/DBHPlaceholder.png'
 import "./HeroCards.css"
+import StudyProgressBar from './ProgressBar'
 
 export function HeroCard(props: {spaceInfo : Object})
 {
@@ -40,21 +41,4 @@ export function HeroCard(props: {spaceInfo : Object})
         </Card>
     )
 
-}
-
-function StudyProgressBar(props: {capacity: number})
-{
-    let barColor = "";
-    if (props.capacity < 35)
-        barColor = "LOW_OCCUPANCY"
-    else if (props.capacity < 75)
-        barColor = "MED_OCCUPANCY"
-    else
-        barColor = "HI_OCCUPANCY"
-
-    return (
-        <>
-            <ProgressBar now={props.capacity} variant={barColor} style={{height: "20px", width: "50%", borderRadius: "20px"}}/>
-        </>
-    )
 }
