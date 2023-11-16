23434db52b6dff4010e8c0598716db8db63fd06e
Bryan Silva <bryanjsilva04@gmail.com>
2023-11-15 18:49:07 -0800

    WTF resolved

---
+++ app/src/components/TopNavBar.tsx
@@ -0,0 +1,23 @@
+import studyZotIcon from '../assets/StudyZotIcon_Circle.svg'
+import './TopNavBar.css'
+
+function Switch() {
+  return (
+    <>
+      <label className="switch">
+        <input type="checkbox"/>
+        <span className="slider"></span>
+      </label>
+    </>
+  )
+}
+
+export function TopNavBar() {
+    return (
+      <div className="navbar">
+        <img src={studyZotIcon}/>
+        <h1> StudyZot </h1>
+        <Switch />
+      </div>
+    )
+}
