4193ae0c4616065805185a3fe768187cd9564578
EightBitByte <apixelatedpug@gmail.com>
2023-11-17 15:10:19 -0800

    Fix build errors in App.tsx, package.json

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,4 @@
 import { TopNavBar } from './components/TopNavBar.tsx'
-import { BasicStudyCard } from './components/SpaceCards.tsx'
-import { HeroCard } from './components/HeroCards.tsx'
 import { LeftSideBar } from './components/LeftSideBar.tsx'
 import { Footer } from './components/Footer.tsx'
 import Container from "react-bootstrap/Container"
