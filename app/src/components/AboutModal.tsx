28f5dd534e2bc86ae8d24ecceef65b9bd1ffc98f
EightBitByte <apixelatedpug@gmail.com>
2023-11-17 12:30:26 -0800

    Enhance about modal closes #62

--- app/src/components/AboutModal.tsx
+++ app/src/components/AboutModal.tsx
@@ -1,4 +1,6 @@
 import Modal from 'react-bootstrap/Modal'
+import StudyZotIcon from '../assets/StudyZotIcon_Circle2.svg'
+import './AboutModal.css'
 
 export function AboutModal(props: AboutProps) {
   return (
@@ -14,13 +16,16 @@ export function AboutModal(props: AboutProps) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>
-          StudyZot is a web application designed by four second-year undergraduate students at the
-          University of California, Irvine. The app is designed to help students find a desirable
-          place to study on UCI campus, based on their current location. StudyZot was built using
-          React.js, TypeScript, HTML, and CSS.<br></br><br></br>
-          Made with ♥ from the StudyZot Team
-        </p>
+        <div className='about-flex'>
+            <img src={StudyZotIcon} className='about-img'/>
+            <p>
+            StudyZot is a web application designed by four second-year undergraduate students at the
+            University of California, Irvine. The app is designed to help students find a desirable
+            place to study on UCI campus, based on their current location. StudyZot was built using
+            React.js, TypeScript, HTML, and CSS.<br></br><br></br>
+            Made with ♥ from the StudyZot Team
+            </p>
+        </div>
       </Modal.Body>
     </Modal>
   )
